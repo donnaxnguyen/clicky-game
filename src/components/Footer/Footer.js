d319f2b6334e7219839829ee662fe67cf8e4e139
donna <donnaxnguyen@yahoo.com>
2020-08-23 20:06:12 -0700

    created container, footer, and navbar. updated background png, tweaked banner

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Donna Nguyen</div>;
+
+export default Footer;
--- src/images.js
+++ src/images.js
@@ -1,7 +1,7 @@
 import alien from './images/alien.png';
 import barney from './images/barney.png';
 import bart from './images/bart.png';
-import burns from './images/burns.png';
+import burns from './images/burns.jpg';
 import duff from './images/duff.png';
 import granddad from './images/granddad.png';
 import homer from './images/homer.png';
@@ -9,7 +9,7 @@ import itchy from './images/itchy.png';
 import krusty from './images/krusty.png';
 import lisa from './images/lisa.png';
 import maggie from './images/maggie.png';
-import scratchy from './images/scratchy.png';
+import scratchy from './images/scratchy.jpg';
 
 const images = [
 alien,
