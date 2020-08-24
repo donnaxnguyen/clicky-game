dcc6ef54b15e260cbb71146011c88fb762df378e
donna <donnaxnguyen@yahoo.com>
2020-08-23 20:16:33 -0700

    adding container and nnavbar.css, created navbar.js and index.. also starting clickygame.js

---
+++ src/components/ClickyGame.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Container from './Container';
+import Footer from './Footer';
+import Banner from './Banner';
+import images from '../images';
