4896d08f0862c32592527e9f374c2ad9b2e254b2
donna <donnaxnguyen@yahoo.com>
2020-08-23 19:36:37 -0700

    adding banner and creating images

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="m-0">Click on an image to start the game! You cannot click on an image twice!</p>
+  </div>
+);
+
+export default Banner;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import alien from './images/alien.png';
+import barney from './images/barney.png';
+import bart from './images/bart.png';
+import burns from './images/burns.png';
+import duff from './images/duff.png';
+import granddad from './images/granddad.png';
+import homer from './images/homer.png';
+import itchy from './images/itchy.png';
+import krusty from './images/krusty.png';
+import lisa from './images/lisa.png';
+import maggie from './images/maggie.png';
+import scratchy from './images/scratchy.png';
+
+const images = [
+alien,
+barney,
+bart,
+burns,
+duff,
+granddad,
+homer,
+itchy,
+krusty,
+lisa,
+maggie,
+scratchy
+];
+
+export default images;
